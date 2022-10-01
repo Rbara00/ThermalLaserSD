68171d202d3573d70bd3f77ee24674f9ad4ef6be
Rob Bara <62036380+Rbara00@users.noreply.github.com>
2022-10-01 12:53:05 -0400

    Update animal_classGUI.py

--- laser_test_GUI/animal_classGUI.py
+++ laser_test_GUI/animal_classGUI.py
@@ -4,8 +4,8 @@
 ## SD Team 10: Robert Bara, Zari Grandy, Ezra Galapo, Tyiana Smith
 ## 
 ## Author: Robert Bara
-## Date:    4/5/2022
-## Version: 2.5
+## Date:    10/1/2022
+## Version: 3
 ##
 ## Description:
 ##  This python file contains the class functions to create an animal data type, obtain data, and analyze the
@@ -22,9 +22,9 @@ from os import stat
 from re import S
 from sre_constants import MAXGROUPS             # Import date class from datetime module
 from statistics import pstdev, pvariance, mean    # Import statistics to utilize mean, standard deviation, and variance calculations
-import RPi.GPIO as GPIO        # Import General-Purpose In/Out for RPI4 to control laser and photodiode
+#import RPi.GPIO as GPIO        # Import General-Purpose In/Out for RPI4 to control laser and photodiode
 from time import time
-from time import sleep          # Import time delay to delay laser start up to prevent false readings
+from time import sleep
 from tkinter import*
 from tkinter import messagebox
 
@@ -93,9 +93,9 @@ class animal:
     def trial_gui(self,placed_label):
         ############################################################################
         #Setup GPIO Board for RPI
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(13,GPIO.OUT)
-        GPIO.output(13,GPIO.LOW)
+       # GPIO.setmode(GPIO.BOARD)
+       # GPIO.setup(13,GPIO.OUT)
+       # GPIO.output(13,GPIO.LOW)
         ############################################################################
 
         #Run a trial and insert withdrawal time into a list for exporting
@@ -103,14 +103,20 @@ class animal:
 
         ############################################################################
         #Clear the GPIO Pins, overide laser to be off
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(13,GPIO.OUT)
-        GPIO.output(13,GPIO.LOW)
+       # GPIO.setmode(GPIO.BOARD)
+       # GPIO.setup(13,GPIO.OUT)
+       # GPIO.output(13,GPIO.LOW)
         ############################################################################
 
         t_1=float(t_1) #Ensure the time is a valid time with decimals
-        #Was the trial valid enough for the user?
-        valid_trial=messagebox.askyesno("Valid Trial","Keep Trial?\nName: " + self.name +" Time: "+str(t_1)+" seconds")
+    
+        #Check if the laser timed out
+        if (t_1==-10):
+            error_msg=messagebox.showerror("Time Out","Error: No reaction detected within 10 Seconds")
+            valid_trial=False
+        #If a value was returned, ask if this is a valid trial
+        else:
+            valid_trial=messagebox.askyesno("Valid Trial","Keep Trial?\nName: " + self.name +" Time: "+str(t_1)+" seconds")
         #If trial is discarded, return back to the test menu
         if valid_trial==False:
                 placed_label.config(text="Photodiode Status:")
@@ -133,56 +139,54 @@ class animal:
         
     #Method for recording withdrawal time with laser
     def timer(self,placed_label):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(11,GPIO.IN)  #PhotoDiode Signal Pin
-        GPIO.setup(13,GPIO.OUT) #Output to the Laser
-        GPIO.output(13,GPIO.LOW) #Initialize to off
-    
-        #print("\tProgram Started")
-        
+       # GPIO.setmode(GPIO.BOARD)
+       # GPIO.setup(11,GPIO.IN)  #PhotoDiode Signal Pin
+       # GPIO.setup(13,GPIO.OUT) #Output to the Laser
+       # GPIO.output(13,GPIO.LOW) #Initialize to off
+
         first_placed=False
-        #print("Searching for Paw")
+        time_out=10
         placed_label.config(text="Photodiode Status: Searching for Paw")
-        #--------------------------------------------------------------
-        
+        #--------------------------------------------------------------       
+        #t_0=time()
+        #while time()<t_0+10:
         #t_1=input("") #REMOVE THIS LINE IN FUTURE THIS IS JUST FOR PROGRAMMING AT HOME
-        
+        #    t_1=time()
         #print("\tPaw Placed time: %s seconds" % t_1)
-
-
-        #return t_1
+        #return t_1-t_0
         #--------------------------------------------------------------
+        
+        #Check if the photo diode is not covered, when it is covered, turn on the laser
         while first_placed is False:
-            GPIO.output(13,GPIO.LOW)
-            if GPIO.input(11)==0:
+            if GPIO.input(11)==0:           #When the Photodiode is first covered
                 first_placed=True
-                GPIO.output(13,GPIO.HIGH) #Turn on the Laser
-                #print("Paw Placed")
-                placed_label.config(text="Photodiode Status: Paw Placed Starting Laser in 3")
-                sleep(1)
-                placed_label.config(text="Photodiode Status: Paw Placed Starting Laser in 2")
-                sleep(1)
-                placed_label.config(text="Photodiode Status: Paw Placed Starting Laser in 1")
-                sleep(1)
-                placed_label.config(text="Photodiode Status: Paw Placed Laser on")
+                GPIO.output(13,GPIO.HIGH)   #Turn on the Laser
+                t_0=time()                  #Get initial time as laser starts
                 continue
         
-        t_0=time()
-        while True:
-            if GPIO.input(11)==0:
-                placed=True
-               
-            else:
-                placed=False
-                GPIO.output(13,0)
+        #Update GUI Label
+        placed_label.config(text="Photodiode Status: Paw Placed, Laser on")
 
+        #Check if the paw is removed and the photo diode becomes uncovered, if so turn off the laser
+        #If nothing happens within 10 seconds, turn off the laser and break from the loop
+        while True and time()<t_0+time_out:
+            if GPIO.input(11)==0:   #check if the photodiode is covered
+                placed=True
+            else:                   #check if the photodiode is uncovered
+                placed=False
+                GPIO.output(13,0)   #Turn off the Laser
+
+            #Check if the photodiode reading is valid, if it is, return the withdrawal time and turn off the laser
             if first_placed is True and placed is False:
                 t_1=time()-t_0
-                #print("Paw Placed time: %s seconds" % t_1)
-                placed_label.config(text="Photodiode Status: Paw Removed Laser Off")
-                GPIO.output(13,GPIO.LOW) #Turn off the laser
-            
+                GPIO.output(13,0) #Turn off the laser
                 break
+        #If the timer timed out, then set t_1 to go to the error message
+        if((time()>t_0+time_out) and (bool(t_1) is False)):
+                t_1=-10
+        #Update GUI Label
+        placed_label.config(text="Photodiode Status: Paw Removed Laser off")
+        
         GPIO.output(13,GPIO.LOW)    #Possibly could comment out
         GPIO.cleanup()              #Could possibly comment out
         return t_1
